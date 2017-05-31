f0270d865890cc5ba0da506be7f1c3147e577531
Vineeth Varghese <vineeth.varghese@gmail.com>
2017-05-31 15:06:20 +1000

    Adding images

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -13,7 +13,7 @@ import FutureInterpreter.ToyState
 
 object Example1 {
   type Stack = Fx.fx3[Toy, ToyState, TimedFuture]
-  implicit val scheduler = Executors.newScheduledThreadPool(1)
+  implicit val scheduler = ExecutorServices.schedulerFromScheduledExecutorService(Executors.newSingleThreadScheduledExecutor())
 
   def main(args: Array[String]): Unit = {
 
@@ -29,82 +29,3 @@ object Example1 {
     ))
   }
 }
-
-// /** No concurrency. Uses Eff.traverseA */
-// object Example01 {
-//   def main(args: Array[String]): Unit = {
-//     type Stack = Fx.fx2[ToyState, Toy]
-
-//     val x = Toy.groupCycleA[Stack](List("a", "b", "c"))
-
-//     // should be List(N/A, N/A, N/A)
-//     println(
-//       ToyStateInterpreter.runToy(x)
-//         .runState(SimState(List.empty, Map.empty))
-//         .run
-//         ._2.actions
-//     )
-//   }
-// }
-
-
-// /** Uses the future effect. */
-// object Example1 {
-//   implicit val sexs = Executors.newScheduledThreadPool(3)
-//   //implicit val ec   = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(5))
-
-//   def main(args: Array[String]): Unit = {
-//     type Stack = Fx.fx3[TimedFuture, ToyState, Toy]
-
-//     val x = Toy.parGroupCycle[Stack](List("a", "b", "c"))
-
-//     // should be List(N/A, N/A, N/A)
-//     println(Await.result(
-//       ToyStateInterpreter.runToy(x)
-//         .runState(SimState(List.empty, Map.empty))
-//         .runAsync,
-//       1 minute
-//     )._2.actions)
-//   }
-// }
-
-// /** Uses future explicitly without the future effect. */
-// object Example2 {
-//   implicit val sexs = Executors.newScheduledThreadPool(3)
-//   //implicit val ec   = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(5))
-
-//   def main(args: Array[String]): Unit = {
-//     type Stack = Fx.fx2[ToyState, Toy]
-
-//     val x = Toy.parGroupCycle2[Stack](List("a", "b", "c"))
-
-//     // should be List(N/A, N/A, N/A)
-//     println(
-//       ToyStateInterpreter.runToy(x)
-//         .runState(SimState(List.empty, Map.empty))
-//         .run
-//         ._2.actions
-//     )
-//   }
-// }
-
-// /** Uses the future effect. */
-// object Example3 {
-//   implicit val sexs = Executors.newScheduledThreadPool(3)
-//   //implicit val ec   = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(5))
-
-//   def main(args: Array[String]): Unit = {
-//     type Stack = Fx.fx3[TimedFuture, ToyState, Toy]
-
-//     val x = Toy.groupCycleA[Stack](List("a", "b", "c"))
-
-//     // should be List(N/A, N/A, N/A)
-//     println(Await.result(
-//       FutureInterpreter.runToy(x)
-//         .runState(SimState(List.empty, Map.empty))
-//         .runAsync,
-//       1 minute
-//     )._2.actions)
-//   }
-// }
-
